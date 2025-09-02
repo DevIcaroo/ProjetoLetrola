f48a72ebc2c878db064618cc9e3a9f7d2635af56
Anna Caroline <carolannago2005@gmail.com>
2025-09-01 23:23:31 -0300

    feat: refatorar Fase1 para incluir background e animar elementos

--- frontend/src/pages/Fase1.jsx
+++ frontend/src/pages/Fase1.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import "../styles/Fase.css";
 
 function Fase1() {
-
   return (
-    <div>
-      <h1>Fase 1</h1>
-      <p>Conteúdo da Fase 1</p>
-    </div>
+    <section className="level-section">
+      <div className="level-container">
+        {/* Fundo fixo */}
+        <img
+          src="./level-1-background.svg"
+          alt="fundo-de-floresta-com-pinheiros-e-gramado"
+          className="level-1-bg"
+        />
+
+        {/* Nuvens animadas */}
+        <div className="clouds-wrapper">
+          <img src="./clouds.svg" alt="nuvens" className="clouds" />
+          <img src="./clouds.svg" alt="nuvens" className="clouds delay" />
+        </div>
+
+        {/* Árvores animadas */}
+        <div className="trees-wrapper">
+          <img src="./trees-transparent.svg" alt="pinheiros" className="pines" />
+          <img src="./trees-transparent.svg" alt="pinheiros" className="pines delay" />
+        </div>
+      </div>
+    </section>
   );
 }
 
