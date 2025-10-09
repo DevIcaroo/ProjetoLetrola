87d6d98cf6784b668946f640c913c44488a27653
Anna Caroline <carolannago2005@gmail.com>
2025-10-09 02:18:31 -0300

    fix: db.get(   `SELECT MAX(fase) as fase_atual FROM progresso WHERE id_jogador = ? AND mundo = ?`,   [id_jogador, mundoRequisitado],   (err, row) => {

--- backend/initDatabase.js
+++ backend/initDatabase.js
@@ -14,7 +14,7 @@ function criarTabelas() {
         return;
       }
       console.log("Tabelas criadas/verificadas com sucesso.");
-      seedDatabase(); 
+      seedDatabase();
     });
   });
 }
@@ -41,47 +41,41 @@ function seedDatabase() {
         { mundo: 1, fase: 1, ordem: 1, personagem_id: 1, fala: 'Olá, bem-vindo ao Mundo 1!', expressao: 'feliz' },
         { mundo: 2, fase: 1, ordem: 1, personagem_id: 2, fala: 'Brrr! Bem-vindo ao meu mundo gelado!', expressao: 'feliz' },
     ];
-    
-    // MUDANÇA: Todas as dicas foram revisadas para serem menos óbvias e mais educativas.
+
     const itensPorFase = [
         // --- Itens do Mundo 1 (Frutas) ---
-        { mundo: 1, fase: 1, nome: 'UVA', dica1: 'Começa com a letra U.', dica2: 'É uma palavra bem curta, com 3 letras.', imagem_url: '/public/uva.svg' },
-        { mundo: 1, fase: 1, nome: 'MAÇÃ', dica1: 'É uma palavra com 4 letras.', dica2: 'Termina com o som de "Ã". Cuidado com o tio (~)!', imagem_url: '/public/maca.svg' },
-        { mundo: 1, fase: 1, nome: 'PERA', dica1: 'Rima com a palavra "ERA".', dica2: 'Começa com a sílaba "PE".', imagem_url: '/public/pera.svg' },
-        { mundo: 1, fase: 2, nome: 'COCO', dica1: 'A letra O aparece duas vezes.', dica2: 'Usa a letra C duas vezes.', imagem_url: '/public/coco.svg' },
-        { mundo: 1, fase: 2, nome: 'CAJU', dica1: 'Termina com a letra U.', dica2: 'A primeira sílaba é "CA".', imagem_url: '/public/caju.svg' },
-        { mundo: 1, fase: 2, nome: 'LIMÃO', dica1: 'Rima com a palavra "MÃO".', dica2: 'Tem um acento tio (~) na letra A.', imagem_url: '/public/limao.svg' },
-        { mundo: 1, fase: 3, nome: 'MANGA', dica1: 'É uma palavra com 5 letras.', dica2: 'Termina com as letras G e A.', imagem_url: '/public/manga.svg' },
-        { mundo: 1, fase: 3, nome: 'MAMÃO', dica1: 'Começa com a sílaba "MA".', dica2: 'Também rima com "MÃO".', imagem_url: '/public/mamao.svg' },
-        { mundo: 1, fase: 3, nome: 'BANANA', dica1: 'Tem 6 letras e 3 delas são a letra A.', dica2: 'Termina com a sílaba "NA".', imagem_url: '/public/banana.svg' },
-        { mundo: 1, fase: 4, nome: 'LARANJA', dica1: 'Começa com "LA" e termina com "JA".', dica2: 'A sílaba do meio é "RAN".', imagem_url: '/public/laranja.svg' },
-        { mundo: 1, fase: 4, nome: 'ABACATE', dica1: 'É uma palavra com 4 sílabas.', dica2: 'Começa com a letra A.', imagem_url: '/public/abacate.svg' },
-        { mundo: 1, fase: 4, nome: 'MORANGO', dica1: 'A sílaba do meio é "RAN".', dica2: 'Começa com "MO" e termina com "GO".', imagem_url: '/public/morango.svg' },
-        { mundo: 1, fase: 5, nome: 'ABACAXI', dica1: 'Começa com "A" e termina com "I".', dica2: 'Escreve-se com X e não com CH.', imagem_url: '/public/abacaxi.svg' },
-        { mundo: 1, fase: 5, nome: 'MELANCIA', dica1: 'Começa com a sílaba "ME".', dica2: 'A sílaba do meio é "LAN".', imagem_url: '/public/melancia.svg' },
-        { mundo: 1, fase: 5, nome: 'MARACUJÁ', dica1: 'Usa a letra Jota (J).', dica2: 'Tem um acento agudo na última letra!', imagem_url: '/public/maracuja.svg' },
-        
+        { mundo: 1, fase: 1, nome: 'UVA', dica1: 'Começa com a letra U.', dica2: 'É uma palavra bem curta, com 3 letras.', imagem_url: '/uva.svg' },
+        { mundo: 1, fase: 1, nome: 'MAÇÃ', dica1: 'É uma palavra com 4 letras.', dica2: 'Termina com o som de "Ã". Cuidado com o tio (~)!', imagem_url: '/maca.svg' },
+        { mundo: 1, fase: 1, nome: 'PERA', dica1: 'Rima com a palavra "ERA".', dica2: 'Começa com a sílaba "PE".', imagem_url: '/pera.svg' },
+        { mundo: 1, fase: 2, nome: 'COCO', dica1: 'A letra O aparece duas vezes.', dica2: 'Usa a letra C duas vezes.', imagem_url: '/coco.svg' },
+        { mundo: 1, fase: 2, nome: 'CAJU', dica1: 'Termina com a letra U.', dica2: 'A primeira sílaba é "CA".', imagem_url: '/caju.svg' },
+        { mundo: 1, fase: 2, nome: 'LIMÃO', dica1: 'Rima com a palavra "MÃO".', dica2: 'Tem um acento tio (~) na letra A.', imagem_url: '/limao.svg' },
+        { mundo: 1, fase: 3, nome: 'MANGA', dica1: 'É uma palavra com 5 letras.', dica2: 'Termina com as letras G e A.', imagem_url: '/manga.svg' },
+        { mundo: 1, fase: 3, nome: 'MAMÃO', dica1: 'Começa com a sílaba "MA".', dica2: 'Também rima com "MÃO".', imagem_url: '/mamao.svg' },
+        { mundo: 1, fase: 3, nome: 'BANANA', dica1: 'Tem 6 letras e 3 delas são a letra A.', dica2: 'Termina com a sílaba "NA".', imagem_url: '/banana.svg' },
+        { mundo: 1, fase: 4, nome: 'LARANJA', dica1: 'Começa com "LA" e termina com "JA".', dica2: 'A sílaba do meio é "RAN".', imagem_url: '/laranja.svg' },
+        { mundo: 1, fase: 4, nome: 'ABACATE', dica1: 'É uma palavra com 4 sílabas.', dica2: 'Começa com a letra A.', imagem_url: '/abacate.svg' },
+        { mundo: 1, fase: 4, nome: 'MORANGO', dica1: 'A sílaba do meio é "RAN".', dica2: 'Começa com "MO" e termina com "GO".', imagem_url: '/morango.svg' },
+        { mundo: 1, fase: 5, nome: 'ABACAXI', dica1: 'Começa com "A" e termina com "I".', dica2: 'Escreve-se com X e não com CH.', imagem_url: '/abacaxi.svg' },
+        { mundo: 1, fase: 5, nome: 'MELANCIA', dica1: 'Começa com a sílaba "ME".', dica2: 'A sílaba do meio é "LAN".', imagem_url: '/melancia.svg' },
+        { mundo: 1, fase: 5, nome: 'MARACUJÁ', dica1: 'Usa a letra Jota (J).', dica2: 'Tem um acento agudo na última letra!', imagem_url: '/maracuja.svg' },
+
         // --- Itens do Mundo 2 (Bebidas) ---
-        // Fase 1
-        { mundo: 2, fase: 1, nome: 'SUCO', dica1: 'Começa com a letra S.', dica2: 'É uma palavra com 4 letras e 2 sílabas.', imagem_url: '/public/suco.svg' },
-        { mundo: 2, fase: 1, nome: 'LEITE', dica1: 'Começa com a letra L.', dica2: 'Termina com a sílaba "TE".', imagem_url: '/public/leite.svg' },
-        { mundo: 2, fase: 1, nome: 'CAFÉ', dica1: 'Tem um acento agudo no final!', dica2: 'Rima com "PÉ".', imagem_url: '/public/cafe.svg' },
-        // Fase 2
-        { mundo: 2, fase: 2, nome: 'ÁGUA', dica1: 'Começa com a letra A e tem acento.', dica2: 'A segunda sílaba é "GUA".', imagem_url: '/public/agua.svg' },
-        { mundo: 2, fase: 2, nome: 'VITAMINA', dica1: 'É uma palavra grande, com 8 letras!', dica2: 'Termina com a sílaba "NA".', imagem_url: '/public/vitamina.svg' },
-        { mundo: 2, fase: 2, nome: 'LIMONADA', dica1: 'Começa com "LI" e termina com "DA".', dica2: 'Tem 4 sílabas.', imagem_url: '/public/limonada.svg' },
-        // Fase 3
-        { mundo: 2, fase: 3, nome: 'SUCO DE MORANGO', dica1: 'São três palavras separadas.', dica2: 'A última palavra é "MORANGO".', imagem_url: '/public/suco_morango.svg' },
-        { mundo: 2, fase: 3, nome: 'LARANJADA', dica1: 'Começa com "LA" e termina com "DA".', dica2: 'É uma palavra com 4 sílabas.', imagem_url: '/public/laranjada.svg' },
-        { mundo: 2, fase: 3, nome: 'CALDO DE CANA', dica1: 'São três palavras e a do meio é "DE".', dica2: 'A última palavra é "CANA".', imagem_url: '/public/caldo_de_cana.svg' },
-        // Fase 4
-        { mundo: 2, fase: 4, nome: 'IOGURTE', dica1: 'É uma palavra com 7 letras.', dica2: 'Termina com a sílaba "TE".', imagem_url: '/public/iogurte.svg' },
-        { mundo: 2, fase: 4, nome: 'ÁGUA DE COCO', dica1: 'A primeira palavra tem acento.', dica2: 'A última palavra é "COCO".', imagem_url: '/public/agua_de_coco.svg' },
-        { mundo: 2, fase: 4, nome: 'ACHOCOLATADO', dica1: 'É uma palavra bem grande!', dica2: 'Começa com "A" e termina com "DO".', imagem_url: '/public/achocolatado.svg' },
-        // Fase 5
-        { mundo: 2, fase: 5, nome: 'REFRIGERANTE', dica1: 'A sílaba do meio é "GE".', dica2: 'Termina com "RANTE".', imagem_url: '/public/refrigerante.svg' },
-        { mundo: 2, fase: 5, nome: 'CHÁ', dica1: 'É uma palavra bem curta, com 3 letras.', dica2: 'Começa com as letras C e H juntas.', imagem_url: '/public/cha.svg' },
-        { mundo: 2, fase: 5, nome: 'CHAMITO', dica1: 'Começa com o som de "CHA".', dica2: 'Termina com a sílaba "TO".', imagem_url: '/public/chamito.svg' },
+        { mundo: 2, fase: 1, nome: 'SUCO', dica1: 'Começa com a letra S.', dica2: 'É uma palavra com 4 letras e 2 sílabas.', imagem_url: '/suco.svg' },
+        { mundo: 2, fase: 1, nome: 'LEITE', dica1: 'Começa com a letra L.', dica2: 'Termina com a sílaba "TE".', imagem_url: '/leite.svg' },
+        { mundo: 2, fase: 1, nome: 'CAFÉ', dica1: 'Tem um acento agudo no final!', dica2: 'Rima com "PÉ".', imagem_url: '/cafe.svg' },
+        { mundo: 2, fase: 2, nome: 'ÁGUA', dica1: 'Começa com a letra A e tem acento.', dica2: 'A segunda sílaba é "GUA".', imagem_url: '/agua.svg' },
+        { mundo: 2, fase: 2, nome: 'VITAMINA', dica1: 'É uma palavra grande, com 8 letras!', dica2: 'Termina com a sílaba "NA".', imagem_url: '/vitamina.svg' },
+        { mundo: 2, fase: 2, nome: 'LIMONADA', dica1: 'Começa com "LI" e termina com "DA".', dica2: 'Tem 4 sílabas.', imagem_url: '/limonada.svg' },
+        { mundo: 2, fase: 3, nome: 'SUCO DE MORANGO', dica1: 'São três palavras separadas.', dica2: 'A última palavra é "MORANGO".', imagem_url: '/suco_morango.svg' },
+        { mundo: 2, fase: 3, nome: 'LARANJADA', dica1: 'Começa com "LA" e termina com "DA".', dica2: 'É uma palavra com 4 sílabas.', imagem_url: '/laranjada.svg' },
+        { mundo: 2, fase: 3, nome: 'CALDO DE CANA', dica1: 'São três palavras e a do meio é "DE".', dica2: 'A última palavra é "CANA".', imagem_url: '/caldo_de_cana.svg' },
+        { mundo: 2, fase: 4, nome: 'IOGURTE', dica1: 'É uma palavra com 7 letras.', dica2: 'Termina com a sílaba "TE".', imagem_url: '/iogurte.svg' },
+        { mundo: 2, fase: 4, nome: 'ÁGUA DE COCO', dica1: 'A primeira palavra tem acento.', dica2: 'A última palavra é "COCO".', imagem_url: '/agua_de_coco.svg' },
+        { mundo: 2, fase: 4, nome: 'ACHOCOLATADO', dica1: 'É uma palavra bem grande!', dica2: 'Começa com "A" e termina com "DO".', imagem_url: '/achocolatado.svg' },
+        { mundo: 2, fase: 5, nome: 'REFRIGERANTE', dica1: 'A sílaba do meio é "GE".', dica2: 'Termina com "RANTE".', imagem_url: '/refrigerante.svg' },
+        { mundo: 2, fase: 5, nome: 'CHÁ', dica1: 'É uma palavra bem curta, com 3 letras.', dica2: 'Começa com as letras C e H juntas.', imagem_url: '/cha.svg' },
+        { mundo: 2, fase: 5, nome: 'CHAMITO', dica1: 'Começa com o som de "CHA".', dica2: 'Termina com a sílaba "TO".', imagem_url: '/chamito.svg' },
     ];
 
     db.serialize(() => {
@@ -98,8 +92,8 @@ function seedDatabase() {
       stmtDialogos.finalize();
 
       const stmtItens = db.prepare("INSERT INTO itens_fase (mundo, fase, ordem, resposta, letras, dica1, dica2, imagem_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-      
-      let ordemContador = {}; 
+
+      let ordemContador = {};
       itensPorFase.forEach((item) => {
         const chaveFase = `${item.mundo}-${item.fase}`;
         if (!ordemContador[chaveFase]) ordemContador[chaveFase] = 1;
@@ -113,9 +107,9 @@ function seedDatabase() {
 
         const jsonPecas = JSON.stringify(pecasEmbaralhadas);
         const ordem = ordemContador[chaveFase];
-        
+
         stmtItens.run(item.mundo, item.fase, ordem, item.nome, jsonPecas, item.dica1, item.dica2, item.imagem_url);
-        
+
         ordemContador[chaveFase]++;
       });
 
