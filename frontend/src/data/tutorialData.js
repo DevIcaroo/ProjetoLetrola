0e766c0a7cfd50532b9bf9d4dcbdc0eec236e987
Anna Caroline <carolannago2005@gmail.com>
2025-10-16 15:23:03 -0300

    feat: adicionar dados de tutorial para os mundos 1 a 4

---
+++ frontend/src/data/tutorialData.js
@@ -0,0 +1,61 @@
+export const tutorials = {
+  // Tutorial para o Mundo 1
+  '1': [
+    {
+      image: '/tutorial-1-1.svg',
+      text: 'Use as setas do teclado para movimentar o Abú pela tela.'
+    },
+    {
+      image: '/tutorial-1-2.svg',
+      text: 'Pule para pegar as frutas.'
+    },
+    {
+      image: '/tutorial-1-3.svg',
+      text: 'Arraste as letras com o mouse para completar a palavra'
+    }
+  ],
+  // Tutorial para o Mundo 2
+  '2': [
+    {
+      image: '/tutorial-2-1.svg',
+      text: 'ATenção! nesta fase a setinha do mouse é um anzol.'
+    },
+    {
+      image: '/tutorial-2-2.svg',
+      text: 'Pesque as bebidas usando a vara de pesca!'
+    },
+    {
+      image: '/tutorial-2-3.svg',
+      text: 'Troque as letras de lugar clicando em duas até formar a palavra.'
+    }
+  ],
+  // Tutorial para o Mundo 3
+  '3': [
+    {
+      image: '/tutorial-3-1.svg',
+      text: 'Ao clicar na dica do sapo os quadrados da cruzadinha indicam onde escrever.'
+    },
+    {
+      image: '/tutorial-3-2.svg',
+      text: 'Preste atenção nas imagens! elas também são dicas!'
+    },
+    { image: '/tutorial-3-3.svg',
+      text: 'Se acertar, a palavra fica verde. se errar os quadrados piscam em vermelho!'
+    }
+  ],
+  // Tutorial para o Mundo 4
+  '4': [
+    {
+      image: '/tutorial-4-1.svg',
+      text: 'Preste atenção no quadrado de dicas do caça-palavras.'
+    },
+    {
+      image: '/tutorial-4-2.svg',
+      text: 'Procure as palaras no painel.'
+    },
+    { 
+      image: '/tutorial-4-3.svg',
+      text: 'clique e arraste o mouse quando encontrar uma sobremesa!'
+    }
+  ],
+};
