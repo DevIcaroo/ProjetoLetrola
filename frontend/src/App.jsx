71d36109dc7a18e08dd69550dedc36787f9e315e
Anna Caroline <carolannago2005@gmail.com>
2025-10-23 03:39:01 -0300

    feat: adicionar caminho para a página de créditos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.jsx";
 import GameMap from "./pages/GameMap.jsx";
 import Fase from "./pages/Fase.jsx"
 import Ajuda from "./pages/Ajuda.jsx"
+import Credits from "./pages/Credits.jsx";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/mapa-do-jogo" element={<GameMap />} />
         <Route path="/mundo/:mundoId/fase/:faseId" element={<Fase />} />
         <Route path="/ajuda" element={<Ajuda />} />
+        <Route path="/creditos" element={<Credits />} />
       </Routes>
     </Router>
   );
